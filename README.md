b411c1f12125ff3195c360acd2f73d6bb0c0886c
Mark Lorenzo <marklorenzo444@gmail.com>
2025-01-03 11:21:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Setup
+```
+$ bundle install
+$ bundle exec rake           # Verifies that the test suite is green
+$ bundle exec rerun app.rb   # Runs the app on http://localhost:4567
+```
+  
---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'rerun'
+require 'mhartl_palindrome'
+
+get '/' do
+  @title = 'Home'
+  erb :index
+end
+
+get '/about' do
+  @title = 'About'
+  erb :about
+end
+
+get '/palindrome' do
+  @title = 'Palindrome Detector'
+  erb :palindrome
+end
+
+post '/check' do
+  @phrase = params['phrase'] || ""
+  erb :result
+end
